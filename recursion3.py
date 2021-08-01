8692de7589d4316fb909224d3ddfa305b0abe366
godhiraj-code <dhirajdas.666@gmail.com>
2021-08-02 01:32:40 +0800

    gcd using recursion

---
+++ recursion3.py
@@ -0,0 +1,9 @@
+# find the gcd of two numbers using recursion
+
+def gcd_of_two_numbers(a, b):
+    assert a>=0 and b >=0 and int(a) == a and int(b) == b, "not supported for negative integers"
+    if b == 0:
+        return a
+    return gcd_of_two_numbers(int(b), int(a % b))
+
+print(f"The GCD of 48, 18 is {gcd_of_two_numbers(48, 18)}")
