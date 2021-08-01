2c025c95419abb15ead18972888bdadbb403f130
godhiraj-code <dhirajdas.666@gmail.com>
2021-08-01 23:34:48 +0800

    sum of digits_recursively

---
+++ recursion1.py
@@ -0,0 +1,22 @@
+# Find sum of digits of a positive integer number using recursion
+# Step 1 : Identify the recursive flow
+# 10  = 1 + 0 = 1 
+# 10 / 10 = 1 and remainder = 0 
+# 54 / 10  = 5 and remainder = 4 
+# 112 / 10 = 11 and remainder = 2 
+# 11/10 = 1 and remainder = 1
+# f(n) = f(n% 10) + f (n/10)
+# Step 2 : Find the base condition
+# Step 3 : Check the all types of input files
+
+def sum_of_digit(n):
+    if int(n / 10) <= 10:
+        if n <= 9:
+            return n
+        return int(n/10)
+    return int(n%10) + sum_of_digit(int(n/10))
+
+print(f"sum of digits : {sum_of_digit(0)}")
+
+
+
